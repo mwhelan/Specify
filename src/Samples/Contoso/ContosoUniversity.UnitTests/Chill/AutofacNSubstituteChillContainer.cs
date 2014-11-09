2c1065d357826a2aebf0a727cf45fb1b93393940
Michael Whelan <mjmwdev@gmail.com>
2014-11-09 11:28:28 +0000

    Added MSTest library

--- src/Samples/Contoso/ContosoUniversity.UnitTests/Chill/AutofacNSubstituteChillContainer.cs
+++ src/Samples/Contoso/ContosoUniversity.UnitTests/Chill/AutofacNSubstituteChillContainer.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using Autofac;
 using Autofac.Builder;
 using Autofac.Core;
-using Chill.Autofac;
 using NSubstitute;
 
 namespace Chill.Autofac
---
+++ src/Specify.MSTest/ScenarioFor.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Specify.MSTest
+{
+    [TestClass]
+    public abstract class ScenarioFor<TSut> : Specify.ScenarioFor<TSut> where TSut : class
+    {
+        [TestMethod]
+        public override void ExecuteTest()
+        {
+            base.ExecuteTest();
+        }
+    }
+
+    [TestClass]
+    public abstract class ScenarioFor<TSut, TStory> : Specify.ScenarioFor<TSut, TStory>
+        where TSut : class
+        where TStory : UserStory
+    {
+        [TestMethod]
+        public override void ExecuteTest()
+        {
+            base.ExecuteTest();
+        }
+    }
+}
