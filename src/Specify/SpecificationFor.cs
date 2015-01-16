d619448fd4ad9558f45209bd25b617b68585d275
Michael Whelan <mjmwdev@gmail.com>
2015-01-16 18:59:32 +0000

    Removed dependency on Chill

--- src/Specify/SpecificationFor.cs
+++ src/Specify/SpecificationFor.cs
@@ -1,37 +1,25 @@
 using System;
-using Chill;
+using Specify.Providers;
 
 namespace Specify
 {
-    public abstract class SpecificationFor<TSubject>
-        : GivenSubject<TSubject>, ISpecification
+    public abstract class SpecificationFor<TSubject> : ISpecification
         where TSubject : class
     {
+        private ISpecifyContainer _container = new AutofacNSubstituteContainer();
+
+        public TSubject Subject { get; set; }
+
+        public T The<T>() where T : class
+        {
+            return _container.Get<T>();
+        }
+
         public virtual Type Story
         {
             get { return typeof(TSubject); }
         }
         public virtual string Title { get { return GetType().Name.Humanize(LetterCasing.Title); } }
-
-        public virtual void ExecuteTest()
-        {
-            TestRunner.Specify(this);
-        }
-    }
-
-    public abstract class SpecificationFor<TSubject, TResult>
-        : GivenSubject<TSubject, TResult>, ISpecification
-        where TSubject : class
-    {
-        public virtual Type Story
-        {
-            get { return typeof (TSubject); }
-        }
-
-        public virtual string Title
-        {
-            get { return GetType().Name.Humanize(LetterCasing.Title); }
-        }
 
         public virtual void ExecuteTest()
         {
