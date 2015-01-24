88fcf8a09e032ecb398105c2abeadf88e8b7c30a
Michael Whelan <mjmwdev@gmail.com>
2015-01-24 06:43:29 +0000

    Configured Specify to target .Net 4.0

---
+++ src/Plugins/Specify.WithXunit/ScenarioFor.cs
@@ -0,0 +1,25 @@
+﻿using Xunit;
+
+namespace Specify.WithXunit
+{
+    public abstract class ScenarioFor<TSut> : Specify.ScenarioFor<TSut> where TSut : class
+    {
+        [Fact]
+        public override void ExecuteTest()
+        {
+            base.ExecuteTest();
+        }
+    }
+
+    public abstract class ScenarioFor<TSut, TStory> : Specify.ScenarioFor<TSut, TStory>
+        where TSut : class
+        where TStory : UserStory
+    {
+        [Fact]
+        public override void ExecuteTest()
+        {
+            base.ExecuteTest();
+        }
+    }
+
+}
---
+++ src/Plugins/Specify.WithXunit/SpecificationFor.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace Specify.WithXunit
+{
+    public abstract class SpecificationFor<TSut> : Specify.SpecificationFor<TSut> where TSut : class
+    {
+        [Fact]
+        public override void ExecuteTest()
+        {
+            base.ExecuteTest();
+        }
+    }
+}
