14039f2e8491d69afb672e8458baa655775abc28
Carl <carl@blinkbox.com>
2015-04-09 13:27:00 +0100

    resolved test failures

--- src/Specify.Tests/Examples/SutFactoryNinjectContainerIntegrationTests.cs
+++ src/Specify.Tests/Examples/SutFactoryNinjectContainerIntegrationTests.cs
@@ -10,10 +10,11 @@ namespace Specify.Tests.Examples
     {
         protected override SutFactory<T> CreateSut<T>()
         {
-            var container = new NinjectContainer(new StandardKernel());
+            var kernel = new StandardKernel();
+            var container = new NinjectContainer(kernel);
             container.Register<IDependency1, Dependency1>();
             container.Register<IDependency2, Dependency2>();
-            container.Register<ConcreteObjectWithMultipleConstructors>();
+            container.Register(new ConcreteObjectWithMultipleConstructors(new Dependency1(), new Dependency2()));
             container.Register<ConcreteObjectWithNoConstructor>();
             return new SutFactory<T>(container);
         }
