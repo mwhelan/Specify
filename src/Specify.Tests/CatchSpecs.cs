565bbc7c14f034d51e0955d2f160860121920fb8
Michael Whelan <mjmwdev@gmail.com>
2015-06-22 14:31:02 +0100

    Added Catch class for handling exceptions.

---
+++ src/Specify.Tests/CatchSpecs.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Specify.Tests
+{
+    using NUnit.Framework;
+
+    using Shouldly;
+
+    public class CatchSpecs
+    {
+        [Test]
+        public void should_return_null_if_no_exception()
+        {
+            Catch.Exception(() =>
+            {
+                var x = 1;
+                x++;
+            })
+                .ShouldBe(null);
+        }
+
+        [Test]
+        public void should_return_exception_from_action()
+        {
+            Catch.Exception(() => { throw new InvalidOperationException(); })
+                .ShouldBeOfType<InvalidOperationException>();
+        }
+    }
+}
