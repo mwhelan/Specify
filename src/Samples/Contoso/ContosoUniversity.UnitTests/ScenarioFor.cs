29d17c49945642211fea9e8c4ea4c505ff3b08b7
Michael Whelan <mjmwdev@gmail.com>
2015-12-21 12:02:12 +0000

    Added NUnit to resolve mysterious failures with xUnit 2.0

--- src/Samples/Contoso/ContosoUniversity.UnitTests/ScenarioFor.cs
+++ src/Samples/Contoso/ContosoUniversity.UnitTests/ScenarioFor.cs
@@ -1,11 +1,11 @@
-﻿using Xunit;
+﻿using NUnit.Framework;
 
 namespace ContosoUniversity.UnitTests
 {
     public abstract class ScenarioFor<TSut> : Specify.ScenarioFor<TSut>
             where TSut : class
     {
-        [Fact]
+        [Test]
         public override void Specify()
         {
             base.Specify();
