39d184acc330c0d3e38020a554905673f08f7b27
Michael Whelan <mjmwdev@gmail.com>
2016-01-18 10:36:55 +0000

    Started work on docs

---
+++ src/Samples/Contoso/ContosoUniversity.AcceptanceTests/SpecifyAutofacBootstrapper.cs
@@ -0,0 +1,27 @@
+﻿using Autofac;
+using Specify.Autofac;
+
+namespace ContosoUniversity.AcceptanceTests
+{
+    /// <summary>
+    /// The startup class to configure Specify with the Autofac container. 
+    /// Make any changes to the default configuration settings in this file.
+    /// </summary>
+    public class SpecifyAutofacBootstrapper : DefaultAutofacBootstrapper
+    {
+        public SpecifyAutofacBootstrapper()
+        {
+            HtmlReport.ReportHeader = "Contoso University";
+            HtmlReport.ReportDescription = "Acceptance Specifications";
+        }
+
+        /// <summary>
+        /// Register any additional items into the Autofac container using the ContainerBuilder or leave it as it is. 
+        /// </summary>
+        /// <param name="builder">The Autofac <see cref="ContainerBuilder"/>.</param>
+        public override void ConfigureContainer(ContainerBuilder builder)
+        {
+            builder.RegisterModule<AcceptanceTestsAutofacModule>();
+        }
+    }
+}
