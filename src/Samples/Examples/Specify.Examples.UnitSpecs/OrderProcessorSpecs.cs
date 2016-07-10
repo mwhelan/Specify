860f4231311dde07931fe363b47279a6e752b7a1
Michael Whelan <mjmwdev@gmail.com>
2016-07-10 09:37:05 +0100

    Created Examples.UnitSpecs project for TinyIoc automocking examples

---
+++ src/Samples/Examples/Specify.Examples.UnitSpecs/OrderProcessorSpecs.cs
@@ -0,0 +1,67 @@
+﻿using NSubstitute;
+using Shouldly;
+using Specify.Examples.OrderProcessing;
+
+namespace Specify.Examples.UnitSpecs
+{
+    public class given
+    {
+        public abstract class the_item_is_available : ScenarioFor<OrderProcessor>
+        {
+            protected void Given_the_item_is_available()
+            {
+                Container.Get<IInventory>().IsQuantityAvailable("TestPart", 10).Returns(true);
+            }
+        }
+    }
+
+    public class processing_an_order : given.the_item_is_available
+    {
+        private OrderResult _result;
+
+        public void When_processing_an_order()
+        {
+            _result = SUT.Process(new Order { PartNumber = "TestPart", Quantity = 10 });
+        }
+
+        public void Then_the_order_is_accepted()
+        {
+            _result.WasAccepted.ShouldBe(true);
+        }
+
+        public void AndThen_it_checks_the_inventory()
+        {
+            Container.Get<IInventory>().Received().IsQuantityAvailable("TestPart", 10);
+        }
+
+        public void AndThen_it_raises_an_order_submitted_event()
+        {
+            Container.Get<IPublisher>().Received().Publish(Arg.Is<OrderSubmitted>(x => x.OrderNumber == _result.OrderNumber));
+        }
+    }
+
+    public class orders_with_a_negative_quantity : given.the_item_is_available
+    {
+        private OrderResult _result;
+
+        protected void When_processing_an_order_with_a_negative_quantity()
+        {
+            _result = SUT.Process(new Order { PartNumber = "TestPart", Quantity = -1 });
+        }
+
+        public void Then_the_order_is_rejected()
+        {
+            _result.WasAccepted.ShouldBe(false);
+        }
+
+        public void AndThen_it_does_not_check_the_inventory()
+        {
+            Container.Get<IInventory>().DidNotReceive().IsQuantityAvailable("TestPart", -1);
+        }
+
+        public void AndThen_it_does_not_raise_an_order_submitted_event()
+        {
+            Container.Get<IPublisher>().DidNotReceive().Publish(Arg.Any<OrderSubmitted>());
+        }
+    }
+}
---
+++ src/Samples/Examples/Specify.Examples.UnitSpecs/ScenarioFor.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Specify.Examples.UnitSpecs
+{
+    public abstract class ScenarioFor<TSut> : Specify.ScenarioFor<TSut>
+            where TSut : class
+    {
+        [Test]
+        public override void Specify()
+        {
+            base.Specify();
+        }
+    }
+}
