ebe002db033464931c3c463a11e5d9257f175093
Michael Whelan <mjmwdev@gmail.com>
2016-07-24 10:55:47 +0100

    Removed redundant failing test for old behaviour

--- src/tests/Specify.Tests/Mocks/MockFactoryTestsFor.cs
+++ src/tests/Specify.Tests/Mocks/MockFactoryTestsFor.cs
@@ -30,19 +30,5 @@ namespace Specify.Tests.Mocks
             var sut = CreateSut(Substitute.For<IFileSystem>());
             sut.MockProviderName.ShouldBe(AssemblyName);
         }
-
-#if NET46
-        [Test]
-        public void should_throw_InvalidOperationException_if_FakeItEasy_version_not_compatible()
-        {
-            var assembly = Assembly.Load(new AssemblyName(AssemblyName));
-            var fileSystem = Substitute.For<IFileSystem>();
-            fileSystem.Load(Arg.Any<string>()).Returns(assembly);
-            fileSystem.GetTypeFrom(Arg.Any<Assembly>(), Arg.Any<string>()).Returns((Type)null);
-
-            Should.Throw<InvalidOperationException>(() => CreateSut(fileSystem))
-                .Message.ShouldStartWith($"Unable to find Type {TypeName} in assembly ");
-        }
-#endif
     }
 }
