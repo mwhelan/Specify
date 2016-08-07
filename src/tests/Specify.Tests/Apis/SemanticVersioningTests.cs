c9fde3ed3fe15cb9043b1be817df4c51ce591fe5
Michael Whelan <mjmwdev@gmail.com>
2016-08-07 12:19:30 +0100

    Added PublcApiGenerator tests

---
+++ src/tests/Specify.Tests/Apis/SemanticVersioningTests.cs
@@ -0,0 +1,25 @@
+﻿#if NET46
+using NUnit.Framework;
+using Shouldly;
+using Specify.Autofac;
+
+namespace Specify.Tests.Apis
+{
+    public class SemanticVersioningTests
+    {
+        [Test]
+        public void specify_has_no_public_api_changes()
+        {
+            var publicApi = PublicApiGenerator.PublicApiGenerator.GetPublicApi(typeof(IScenario).Assembly);
+            publicApi.ShouldMatchApproved();
+        }
+
+        [Test]
+        public void specify_autofac_has_no_public_api_changes()
+        {
+            var publicApi = PublicApiGenerator.PublicApiGenerator.GetPublicApi(typeof(AutofacContainer).Assembly);
+            publicApi.ShouldMatchApproved();
+        }
+    }
+}
+#endif
