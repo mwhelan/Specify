1cdbd1d02a0daa604f145716979f7ffe6578d893
Shayne van Asperen <shaynevanasperen@gmail.com>
2017-07-06 17:13:32 +0100

    Failing test for resolving a class from the container

--- src/Samples/Specify.Samples/Domain/Atm/Card.cs
+++ src/Samples/Specify.Samples/Domain/Atm/Card.cs
@@ -1,19 +1,21 @@
 namespace Specify.Samples.Domain.Atm
 {
-    public class Card
-    {
-        public int AccountBalance { get; set; }
-        private readonly bool _enabled;
+	public class Card
+	{
+		public virtual int AccountBalance { get; set; }
+		private readonly bool _enabled;
 
-        public Card(bool enabled, int accountBalance)
-        {
-            AccountBalance = accountBalance;
-            _enabled = enabled;
-        }
+		public Card(bool enabled, int accountBalance)
+		{
+			AccountBalance = accountBalance;
+			_enabled = enabled;
+		}
 
-        public bool Enabled
-        {
-            get { return _enabled; }
-        }
-    }
+		protected Card() { }
+
+		public virtual bool Enabled
+		{
+			get { return _enabled; }
+		}
+	}
 }
